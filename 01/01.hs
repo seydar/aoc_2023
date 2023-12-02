a0b48df4bfea1bb72d2a30e6272d2d3716007a1b
Ari Brown <ari@airintech.com>
2023-12-02 11:40:25 -0500

    fixed haskell day 1 part 1, finished day 1 part 2

--- 01/01.hs
+++ 01/01.hs
@@ -9,40 +9,70 @@ getMap = do
   inp <- getContents
   return $ lines inp
 
+digits = map (:[]) ['1'..'9']
+digitsAndWords = digits ++ ["one",
+                            "two",
+                            "three",
+                            "four",
+                            "five",
+                            "six",
+                            "seven",
+                            "eight",
+                            "nine"]
+
 partOne :: [String] -> Int
-partOne = fromMaybe 0 . fmap sum . traverse (ints . firstAndLast)
-  where
-    ints :: [Maybe Char] -> Maybe Int
-    ints = liftM read . sequence
+partOne = fromMaybe 0 . fmap sum . traverse (intsFrom . firstAndLast digits)
 
-partTwo parts = ""
+partTwo :: [String] -> Int
+partTwo = fromMaybe 0 . fmap sum . traverse (intsFrom . firstAndLast digitsAndWords)
 
-firstAndLast :: String -> [Maybe Char]
-firstAndLast str = [firstDigit str ['1'..'9'], lastDigit str ['1'..'9']]
+intsFrom :: [Maybe String] -> Maybe Int
+intsFrom = liftM read . sequence . map convert
 
-firstDigit :: String -> [Char] -> Maybe Char
+convert :: Maybe String -> Maybe Char
+convert Nothing           = Nothing
+convert (Just s) | elem s digits = Just (s !! 0)
+                 | s == "one"    = Just '1'
+                 | s == "two"    = Just '2'
+                 | s == "three"  = Just '3'
+                 | s == "four"   = Just '4'
+                 | s == "five"   = Just '5'
+                 | s == "six"    = Just '6'
+                 | s == "seven"  = Just '7'
+                 | s == "eight"  = Just '8'
+                 | s == "nine"   = Just '9'
+
+firstAndLast :: [String] -> String -> [Maybe String]
+firstAndLast check str = [firstDigit str check, lastDigit str check]
+
+firstDigit :: String -> [String] -> Maybe String
 firstDigit str []     = Nothing
 firstDigit str (i:is) = Just $ first i (firstDigit str is)
   where
     first a Nothing  = a
-    first a (Just b) = lowest (a, (elemIndex a str)) (b, (elemIndex b str))
+    first a (Just b) = lowest (a, (substringIndex a str)) (b, (substringIndex b str))
 
-    lowest (_, Nothing) (b, j) = b
-    lowest (a, i) (b, Nothing) = a
-    lowest (a, i) (b, j)       | i < j     = a
-                               | otherwise = b
 
-lastDigit :: String -> [Char] -> Maybe Char
+lastDigit :: String -> [String] -> Maybe String
 lastDigit str []     = Nothing
 lastDigit str (i:is) = Just $ last i (lastDigit str is)
   where
-    last a Nothing  = a
-    last a (Just b) = biggest (a, (elemIndex a str)) (b, (elemIndex b str))
+    rts = reverse str
 
-    biggest (_, Nothing) (b, j) = b
-    biggest (a, i) (b, Nothing) = a
-    biggest (a, i) (b, j)       | i < j     = b
-                                | otherwise = a
+    last a Nothing  = a
+    last a (Just b) = lowest (a, (substringIndex (reverse a) rts)) (b, (substringIndex (reverse b) rts))
+
+lowest (_, Nothing) (b, j) = b
+lowest (a, i) (b, Nothing) = a
+lowest (a, i) (b, j)       | i < j     = a
+                           | otherwise = b
+
+substringIndex :: String -> String -> Maybe Int
+substringIndex sub str = substringIndex' sub str 0
+  where
+    substringIndex' sub ""  i = Nothing 
+    substringIndex' sub str i | isPrefixOf sub str = Just i
+                              | otherwise          = substringIndex' sub (tail str) (i + 1)
 
 main = do
   args  <- getArgs
