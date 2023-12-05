4bf0ad2b0a774bf78ee56ee99802ff73b594fbde
Ari Brown <ari@airintech.com>
2023-12-05 15:12:40 -0500

    initial solution to ruby day 4, but it's too slow on actual input

---
+++ 04/04.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+class Card
+  attr_accessor :id
+  attr_accessor :winning
+  attr_accessor :have
+
+  def initialize(line)
+    parse_input line
+  end
+
+  def parse_input(line)
+    id, nums = line.split ': '
+
+    @id = id.split(' ')[1].to_i
+
+    @winning, @have = nums.split(' | ').map {|r| r.split(' ').map(&:to_i) }
+  end
+
+  def matching
+    winning & have
+  end
+
+  def score
+    matching.empty? ? 0 : 2 ** (matching.size - 1)
+  end
+
+  def inspect
+    "#<Card: #{id}>"
+  end
+end
+
+def parse_cards(inp)
+  inp.split("\n").map {|l| Card.new l }
+end
+
+def part_one(cards)
+  cards.sum {|c| c.score }
+end
+
+def part_two(cards)
+  i = 0
+  until cards[i].nil?
+    cur = cards[i]
+
+    extra = cards[cur.id, cur.matching.size]
+    cards += extra
+
+    i += 1
+  end
+
+  cards.size
+end
+
+cards = parse_cards STDIN.read
+
+case ARGV[0]
+when "one"
+  p part_one(cards)
+when "two"
+  p part_two(cards)
+end
+
